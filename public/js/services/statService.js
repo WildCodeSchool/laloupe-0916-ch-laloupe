7bca5c5489ed5035661a3258fc989f5bd3c2d511
CamJr <cfontainef1@gmail.com>
2016-11-02 14:49:35 +0100

    services

--- public/js/services/statService.js
+++ public/js/services/statService.js
@@ -22,8 +22,14 @@ class statService {
           texte2: data.texte2,
           texte3: data.texte3,
           texte4: data.texte4,
-          texte5: data.texte5
-
+          texte5: data.texte5,
+          texte6: data.texte6,
+          texte7: data.texte7,
+          texte8: data.texte8,
+          texte9: data.texte9,
+          texte10: data.texte10,
+          texte11: data.texte11,
+          texte12: data.texte12
         });
     }
 
