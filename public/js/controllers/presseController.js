d2fe95de92f811c0f003a152f949b6464e841c6e
Jean-Baptiste Pasquier <contact@jbpasquier.eu>
2016-11-23 11:15:40 +0100

    Update presseController.js

--- public/js/controllers/presseController.js
+++ public/js/controllers/presseController.js
@@ -1,29 +1,27 @@
-class presseController {
+function presseController(presseService) {
 
-    constructor(presseService) {
-        this.presseService = presseService;
-        this.load();
-        $(document).ready(function() {
-            $('.collapsible').collapsible({
-                accordion: false
-            });
-            $(document).ready(function() {
-                $('select').material_select();
-            });
-
+    this.presseService = presseService;
+    $(document).ready(function() {
+        $('.collapsible').collapsible({
+            accordion: false
         });
-        this.jsScrollTo = function(id) { // Au clic sur un élément
-            var page = '#'+id; // Page cible
-            var speed = 500; // Durée de l'animation (en ms)
-            $('html, body').animate({
-                scrollTop: $(page).offset().top - 100
-            }, speed); // Go
-            return false;
-        };
-    }
-    load() {
+        $(document).ready(function() {
+            $('select').material_select();
+        });
+
+    });
+    this.jsScrollTo = function(id) { // Au clic sur un élément
+        var page = '#'+id; // Page cible
+        var speed = 500; // Durée de l'animation (en ms)
+        $('html, body').animate({
+            scrollTop: $(page).offset().top - 100
+        }, speed); // Go
+        return false;
+    };
+    this.load = () => {
         this.presseService.getAll().then((res) => {
             this.presses = res.data;
         });
     }
+    this.load();
 }
