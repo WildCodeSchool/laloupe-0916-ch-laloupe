3e570f20fe09a5da6e81b6c735054a605ad65471
Jean-Baptiste Pasquier <contact@jbpasquier.eu>
2016-11-23 11:14:19 +0100

    Update marcheController.js

--- public/js/controllers/marcheController.js
+++ public/js/controllers/marcheController.js
@@ -1,23 +1,20 @@
-class marcheController {
-
-    constructor(marcheService) {
-        this.marcheService = marcheService;
-        this.load();
-        $(document).ready(function() {
-            $('.collapsible').collapsible({
-                accordion: false
-            });
-            $(document).ready(function() {
-                $('select').material_select();
-            });
-
+function marcheController(marcheService) {
+    this.marcheService = marcheService;
+    $(document).ready(function() {
+        $('.collapsible').collapsible({
+            accordion: false
         });
-    }
+        $(document).ready(function() {
+            $('select').material_select();
+        });
 
-    load(){
+    });
+
+    this.load = () => {
       this.marcheService.getAll().then((res) => {
         this.marche = res.data;
         this.marches = res.data[0];
       });
     }
+    this.load();
   }
