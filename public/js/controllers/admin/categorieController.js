1653aaa028dbb4c80c9cab9445066b94abf6f5bc
Jean-Baptiste Pasquier <contact@jbpasquier.eu>
2016-11-23 11:23:48 +0100

    Update categorieController.js

--- public/js/controllers/admin/categorieController.js
+++ public/js/controllers/admin/categorieController.js
@@ -1,41 +1,38 @@
-class categorieController {
+function categorieController(categorieService, $routeParams) {
 
-    constructor(categorieService, $routeParams) {
-            this.$routeParams = $routeParams;
-            this.tinymceOptions = {
-                toolbar: "forecolor | insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
-                plugins: 'advlist fullscreen autolink link image lists charmap autoresize textcolor'
-            };
-              this.categorie = {
-                "idparent":""
-              };
-            this.categorieService = categorieService;
-            this.load();
+    this.$routeParams = $routeParams;
+    this.tinymceOptions = {
+        toolbar: "forecolor | insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+        plugins: 'advlist fullscreen autolink link image lists charmap autoresize textcolor'
+    };
+      this.categorie = {
+        "idparent":""
+      };
+    this.categorieService = categorieService;
 
-            function uploadFile(file) {
-                var url = '/api/picture';
-                var xhr = new XMLHttpRequest();
-                var fd = new FormData();
-                xhr.open("POST", url, true);
-                xhr.onreadystatechange = function() {
-                    if (xhr.readyState == 4 && xhr.status == 200) {
-                        // Every thing ok, file uploaded
-                        console.log(xhr.responseText); // handle response.
-                    }
-                };
-                fd.append("upload_file", file);
-                xhr.send(fd);
+    function uploadFile(file) {
+        var url = '/api/picture';
+        var xhr = new XMLHttpRequest();
+        var fd = new FormData();
+        xhr.open("POST", url, true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                // Every thing ok, file uploaded
+                console.log(xhr.responseText); // handle response.
             }
-
-            var uploadfiles = document.querySelector('#uploadImage');
-            uploadfiles.addEventListener('change', function() {
-                var files = this.files;
-                for (var i = 0; i < files.length; i++) {
-                    uploadFile(this.files[i]); // call the function to upload the file
-                }
-            }, false);
+        };
+        fd.append("upload_file", file);
+        xhr.send(fd);
     }
-    showService() {
+
+    var uploadfiles = document.querySelector('#uploadImage');
+    uploadfiles.addEventListener('change', function() {
+        var files = this.files;
+        for (var i = 0; i < files.length; i++) {
+            uploadFile(this.files[i]); // call the function to upload the file
+        }
+    }, false);
+    this.showService = () => {
       $(".serviceUpdate").hide();
       var select = document.getElementById("serviceSelect");
       var choice = select.selectedIndex;
@@ -44,17 +41,17 @@ class categorieController {
       $("#" + valeur).show();
     }
 
-    uploadFileChangeImage() {
+    this.uploadFileChangeImage = () => {
       this.UploadImg = '/uploads/img_' + document.getElementById('uploadImage').value.split(/(\|\/)/g).pop().replace('C:\\fakepath\\', '');
       console.log(document.getElementById('uploadImage').value);
     }
 
-    uploadFileChangeImageUpdate(index) {
+    this.uploadFileChangeImageUpdate = (index) => {
       this.UploadImgUpdate = '/uploads/img_' + document.getElementById('uploadImageUpdate' + index).value.split(/(\|\/)/g).pop().replace('C:\\fakepath\\', '');
       console.log(this.UploadImgUpdate);
     }
 
-    load() {
+    this.load = () => {
       this.categorieService.getAll().then((res) => {
           this.categories = res.data;
           var ServiceSelect = document.getElementById('serviceSelect').value;
@@ -62,7 +59,7 @@ class categorieController {
       });
     }
 
-    create(categorie) {
+    this.create = (categorie) => {
       var urlImage = '/uploads/img_' + document.getElementById('uploadImage').value.split(/(\|\/)/g).pop().replace('C:\\fakepath\\', '');
         this.categorie.photo = urlImage;
         console.log(categorie.position);
@@ -75,15 +72,16 @@ class categorieController {
         });
     }
 
-    update(categorie) {
+    this.update = (categorie) => {
         this.categorieService.update(categorie._id, categorie).then(() => {
             this.load();
         });
     }
 
-    delete(categorie) {
+    this.delete = (categorie) => {
         this.categorieService.delete(categorie._id).then(() => {
             this.load();
         });
     }
+    this.load();
 }
