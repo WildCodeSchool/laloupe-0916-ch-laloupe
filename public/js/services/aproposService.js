8212c76f335d098f5e81fe6562c3278866b76b3d
Jean-Baptiste Pasquier <contact@jbpasquier.eu>
2016-11-23 11:39:07 +0100

    Update aproposService.js

--- public/js/services/aproposService.js
+++ public/js/services/aproposService.js
@@ -1,30 +1,25 @@
-class aproposService {
+function aproposService ($http){
 
-    constructor($http) {
-        this.$http = $http;
-    }
+        return {
+            create: (data) => {
+                return $http.post('/api/aproposs', data);
+            },
 
-    create(data) {
-        return this.$http.post('/api/aproposs', data);
-    }
+            getAll: () => {
+                return $http.get('/api/aproposs');
+            },
 
-    getAll() {
-        return this.$http.get('/api/aproposs');
-    }
-
-    getOne(id) {
-        return this.$http.get('/api/aproposs/' + id);
-    }
-
-    update(id, data) {
-        return this.$http.put('/api/aproposs/' + id, {
-          titre: data.titre,
-          texte: data.texte
-        });
-    }
-
-    delete(id) {
-        return this.$http.delete('/api/aproposs/' + id);
-    }
-
+            getOne: (id) => {
+                return $http.get('/api/aproposs/' + id);
+            },
+            update: (id, data) => {
+                return $http.put('/api/aproposs/' + id, {
+                  titre: data.titre,
+                  texte: data.texte
+                });
+            },
+            delete: (id) => {
+                return $http.delete('/api/aproposs/' + id);
+            }
+        }
 }
