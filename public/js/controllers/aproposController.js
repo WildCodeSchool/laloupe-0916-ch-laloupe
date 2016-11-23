57d322d04a7653837d668dd6a6dcb4aefea8a874
Jean-Baptiste Pasquier <contact@jbpasquier.eu>
2016-11-23 11:08:15 +0100

    Update aproposController.js

--- public/js/controllers/aproposController.js
+++ public/js/controllers/aproposController.js
@@ -1,29 +1,26 @@
-class aproposController {
-
-    constructor(aproposService) {
-        this.aproposService = aproposService;
-        this.load();
-        $(document).ready(function() {
-            $('.collapsible').collapsible({
-                accordion: false
-            });
-            $(document).ready(function() {
-                $('select').material_select();
-            });
-
+function aproposController(aproposService) {
+    this.aproposService = aproposService;
+    $(document).ready(function() {
+        $('.collapsible').collapsible({
+            accordion: false
         });
-        this.jsScrollTo = function(id) { // Au clic sur un élément
-            var page = '#'+id; // Page cible
-            var speed = 500; // Durée de l'animation (en ms)
-            $('html, body').animate({
-                scrollTop: $(page).offset().top - 100
-            }, speed); // Go
-            return false;
-        };
-    }
+        $(document).ready(function() {
+            $('select').material_select();
+        });
+
+    });
+    this.jsScrollTo = function(id) { // Au clic sur un élément
+        var page = '#'+id; // Page cible
+        var speed = 500; // Durée de l'animation (en ms)
+        $('html, body').animate({
+            scrollTop: $(page).offset().top - 100
+        }, speed); // Go
+        return false;
+    };
     load() {
         this.aproposService.getAll().then((res) => {
             this.aproposs = res.data;
         });
     }
+    this.load();
 }
