b751c684a05fdcc5776652933f4526c8aeb5ba1d
Jean-Baptiste Pasquier <contact@jbpasquier.eu>
2016-11-23 11:09:55 +0100

    Update contactController.js

--- public/js/controllers/contactController.js
+++ public/js/controllers/contactController.js
@@ -1,27 +1,23 @@
-class contactController {
-
-    constructor(contactService) {
-        this.contactService = contactService;
-        this.load();
-        $(document).ready(function() {
-            $('.collapsible').collapsible({
-                accordion: false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
-            });
-            $(document).ready(function() {
-                $('select').material_select();
-            });
-
+function contactController(contactService) {
+    this.contactService = contactService;
+    $(document).ready(function() {
+        $('.collapsible').collapsible({
+            accordion: false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
         });
-    }
-    load() {
+        $(document).ready(function() {
+            $('select').material_select();
+        });
+
+    });
+    this.load = () => {
         this.contactService.getAll().then((res) => {
             this.contacts = res.data;
             console.log(this.contacts);
         });
     }
 
-    sendEmail() {
+    this.sendEmail = () => {
       this.contactService.sendMail(this.contact);
     }
-
+    this.load();
 }
