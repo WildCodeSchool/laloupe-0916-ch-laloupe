20d9f0566c34d313d1c17a8f9d61c543dc04e92b
Jean-Baptiste Pasquier <contact@jbpasquier.eu>
2016-11-23 11:44:14 +0100

    Update hospitaliseService.js

--- public/js/services/hospitaliseService.js
+++ public/js/services/hospitaliseService.js
@@ -1,34 +1,33 @@
-class hospitaliseService {
+function hospitaliseService($http) {
 
-    constructor($http) {
-        this.$http = $http;
+    return {
+
+        this.create: (data) =>{
+            return this.$http.post('/api/btn2s', data )
+        },
+
+        this.getAll: () =>{
+            return this.$http.get('/api/btn2s');
+        },
+
+        this.getOne: (id) =>{
+            return this.$http.get('/api/btn2s/' + id)
+        },
+
+        this.update: (id, data) =>{
+            return this.$http.put('/api/btn2s/' + id, {
+              title1: data.title1,
+              title2: data.title2,
+              title3: data.title3,
+              texte1: data.texte1,
+              texte2: data.texte2,
+              texte3: data.texte3
+            })
+        },
+
+        this.delete: (id) =>{
+            return this.$http.delete('/api/btn2s/' + id)
+        }
+
     }
-
-    create(data) {
-        return this.$http.post('/api/btn2s', data )
-    }
-
-    getAll() {
-        return this.$http.get('/api/btn2s');
-    }
-
-    getOne(id) {
-        return this.$http.get('/api/btn2s/' + id)
-    }
-
-    update(id, data) {
-        return this.$http.put('/api/btn2s/' + id, {
-          title1: data.title1,
-          title2: data.title2,
-          title3: data.title3,
-          texte1: data.texte1,
-          texte2: data.texte2,
-          texte3: data.texte3
-        })
-    }
-
-    delete(id) {
-        return this.$http.delete('/api/btn2s/' + id)
-    }
-
 }
