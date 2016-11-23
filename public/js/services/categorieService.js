81ec33ed0a232f2466504f149cb76d35c66c4837
Jean-Baptiste Pasquier <contact@jbpasquier.eu>
2016-11-23 11:40:56 +0100

    Update categorieService.js

--- public/js/services/categorieService.js
+++ public/js/services/categorieService.js
@@ -1,47 +1,47 @@
 
-class categorieService {
+function categorieService($http) {
 
-    constructor($http) {
-        this.$http = $http;
-    }
+    return {
 
-    create(data) {
-        return this.$http.post('/api/categories', {
-          titre: data.titre,
-          photo: data.photo,
-          texte: data.texte,
-          lien: data.lien,
-          idparent: data.idparent,
-          position: data.position
+        create: (data) => {
+            return $http.post('/api/categories', {
+              titre: data.titre,
+              photo: data.photo,
+              texte: data.texte,
+              lien: data.lien,
+              idparent: data.idparent,
+              position: data.position
 
-        });
-    }
+            });
+        },
 
-    getAll() {
-        return this.$http.get('/api/categories');
-    }
+        getAll: () => {
+            return $http.get('/api/categories');
+        },
 
-    getOne(id) {
-        return this.$http.get('/api/categories/' + id);
-    }
+        getOne: (id) => {
+            return $http.get('/api/categories/' + id);
+        },
 
-    getChildrenOf(idparent) {
-        return this.$http.get('/api/categories/by-parent/' + idparent);
-    }
+        getChildrenOf: (idparent) => {
+            return $http.get('/api/categories/by-parent/' + idparent);
+        },
 
-    update(id, data) {
-        return this.$http.put('/api/categories/' + id, {
-          titre: data.titre,
-          photo: data.photo,
-          texte: data.texte,
-          lien: data.lien,
-          idparent: data.idparent,
-          position: data.position
-        });
-    }
+        update: (id, data) => {
+            return $http.put('/api/categories/' + id, {
+              titre: data.titre,
+              photo: data.photo,
+              texte: data.texte,
+              lien: data.lien,
+              idparent: data.idparent,
+              position: data.position
+            });
+        },
 
-    delete(id) {
-        return this.$http.delete('/api/categories/' + id);
+        delete: (id) => {
+            return $http.delete('/api/categories/' + id);
+        }
+
     }
 
 }
