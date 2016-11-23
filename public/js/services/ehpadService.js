eeb7462d884c05b9cf74dbb4d26fd93034143aa5
Jean-Baptiste Pasquier <contact@jbpasquier.eu>
2016-11-23 11:50:31 +0100

    Update ehpadService.js

--- public/js/services/ehpadService.js
+++ public/js/services/ehpadService.js
@@ -2,19 +2,19 @@ function ehpadService($http) {
 
     return {
 
-        this.create: (data) => {
+        create: (data) => {
             return $http.post('/api/btn3s', data )
         },
 
-        this.getAll: () => {
+        getAll: () => {
             return $http.get('/api/btn3s');
         },
 
-        this.getOne: (id) => {
+        getOne: (id) => {
             return $http.get('/api/btn3s/' + id)
         },
 
-        this.update: (id, data) => {
+        update: (id, data) => {
             return $http.put('/api/btn3s/' + id, {
               title1: data.title1,
               title2: data.title2,
@@ -27,7 +27,7 @@ function ehpadService($http) {
             })
         },
 
-        this.delete: (id) => {
+        delete: (id) => {
             return $http.delete('/api/btn3s/' + id)
         }
 
