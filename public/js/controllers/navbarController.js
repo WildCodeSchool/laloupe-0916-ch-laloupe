9540f642f28427bd425f01a860981152fbabe2f8
Jean-Baptiste Pasquier <contact@jbpasquier.eu>
2016-11-23 11:15:03 +0100

    Update navbarController.js

--- public/js/controllers/navbarController.js
+++ public/js/controllers/navbarController.js
@@ -1,38 +1,35 @@
-class navbarController {
+function navbarController(categorieService) {
 
-    constructor(categorieService) {
-        this.categorieService = categorieService;
-        this.loadCategories();
+    this.categorieService = categorieService;
 
-        $(window).scroll(function() {
-            if ($(window).scrollTop() > $('#scrollspy').height()) {
-                $('#push').addClass('navbar-fixed');
-                  $('#urgenceHide').addClass('ng-hide');
-                  $('.group-btn').show();
-                $('#topFixed').css({
-                    'margin-top': '64px'
-                });
-            }
-            if ($(window).scrollTop() < $('#scrollspy').height()) {
-                $('#push').removeClass('navbar-fixed');
-                $('#topFixed').css({
-                    'margin-top': '0'
-                });
-            }
-        });
-        this.isToggled = false;
-        this.toggleEmergencies = () => {
-            this.isToggled = !this.isToggled;
-            console.log(this.isToggled);
-            if (this.isToggled === true) {
-              $('.group-btn').hide();
-            } else {
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > $('#scrollspy').height()) {
+            $('#push').addClass('navbar-fixed');
+              $('#urgenceHide').addClass('ng-hide');
               $('.group-btn').show();
-            }
-        };
-    }
+            $('#topFixed').css({
+                'margin-top': '64px'
+            });
+        }
+        if ($(window).scrollTop() < $('#scrollspy').height()) {
+            $('#push').removeClass('navbar-fixed');
+            $('#topFixed').css({
+                'margin-top': '0'
+            });
+        }
+    });
+    this.isToggled = false;
+    this.toggleEmergencies = () => {
+        this.isToggled = !this.isToggled;
+        console.log(this.isToggled);
+        if (this.isToggled === true) {
+          $('.group-btn').hide();
+        } else {
+          $('.group-btn').show();
+        }
+    };
 
-    loadCategories() {
+    this.loadCategories = () => {
         this.categorieService.getChildrenOf('0').then((res) => {
             this.categories = res.data;
             setTimeout(function() {
@@ -40,5 +37,6 @@ class navbarController {
             }, 0);
         });
     }
+    this.loadCategories();
 
 }
