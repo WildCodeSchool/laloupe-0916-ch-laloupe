96e2a56a82cd5a3b3b47a36090b96035785864ec
Jean-Baptiste Pasquier <contact@jbpasquier.eu>
2016-11-23 11:07:36 +0100

    Update actualiteController.js

--- public/js/controllers/actualiteController.js
+++ public/js/controllers/actualiteController.js
@@ -1,28 +1,25 @@
-class actualiteController {
-
-    constructor(actualiteService) {
-        this.actualiteService = actualiteService;
-        this.load();
-        $(document).ready(function() {
-            $('.collapsible').collapsible({
-                accordion: false
-            });
-            $(document).ready(function() {
-                $('select').material_select();
-            });
+function actualiteController(actualiteService) {
+    this.actualiteService = actualiteService;
+    $(document).ready(function() {
+        $('.collapsible').collapsible({
+            accordion: false
         });
-        this.jsScrollTo = function(id) { // Au clic sur un élément
-            var page = '#'+id; // Page cible
-            var speed = 500; // Durée de l'animation (en ms)
-            $('html, body').animate({
-                scrollTop: $(page).offset().top - 100
-            }, speed); // Go
-            return false;
-        };
-    }
+        $(document).ready(function() {
+            $('select').material_select();
+        });
+    });
+    this.jsScrollTo = function(id) { // Au clic sur un élément
+        var page = '#'+id; // Page cible
+        var speed = 500; // Durée de l'animation (en ms)
+        $('html, body').animate({
+            scrollTop: $(page).offset().top - 100
+        }, speed); // Go
+        return false;
+    };
     load() {
         this.actualiteService.getAll().then((res) => {
             this.actualites = res.data;
         });
     }
+    this.load();
 }
