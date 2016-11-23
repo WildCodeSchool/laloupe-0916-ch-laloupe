753b941fd444f6cf46df6ef2f29d14a18b1ea4e1
Jean-Baptiste Pasquier <contact@jbpasquier.eu>
2016-11-23 11:43:09 +0100

    Update ehpadService.js

--- public/js/services/ehpadService.js
+++ public/js/services/ehpadService.js
@@ -1,36 +1,35 @@
-class ehpadService {
+function ehpadService($http) {
 
-    constructor($http) {
-        this.$http = $http;
+    return {
+
+        this.create: (data) => {
+            return $http.post('/api/btn3s', data )
+        }
+
+        this.getAll: () => {
+            return $http.get('/api/btn3s');
+        }
+
+        this.getOne: (id) => {
+            return $http.get('/api/btn3s/' + id)
+        }
+
+        this.update: (id, data) => {
+            return $http.put('/api/btn3s/' + id, {
+              title1: data.title1,
+              title2: data.title2,
+              title3: data.title3,
+              title4: data.title4,
+              texte1: data.texte1,
+              texte2: data.texte2,
+              texte3: data.texte3,
+              texte4: data.texte4
+            })
+        }
+
+        this.delete: (id) => {
+            return $http.delete('/api/btn3s/' + id)
+        }
+
     }
-
-    create(data) {
-        return this.$http.post('/api/btn3s', data )
-    }
-
-    getAll() {
-        return this.$http.get('/api/btn3s');
-    }
-
-    getOne(id) {
-        return this.$http.get('/api/btn3s/' + id)
-    }
-
-    update(id, data) {
-        return this.$http.put('/api/btn3s/' + id, {
-          title1: data.title1,
-          title2: data.title2,
-          title3: data.title3,
-          title4: data.title4,
-          texte1: data.texte1,
-          texte2: data.texte2,
-          texte3: data.texte3,
-          texte4: data.texte4
-        })
-    }
-
-    delete(id) {
-        return this.$http.delete('/api/btn3s/' + id)
-    }
-
 }
